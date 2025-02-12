8b78bfa08e05e6ad3cfa8819891ec1cb20a16b8a
Tony Li <tony.li@automattic.com>
2025-02-12 21:06:38 +1300

    Add a new screen to for browsing and adding new plugins

--- Modules/Sources/WordPressCore/Plugins/PluginDirectoryDataStore.swift
+++ Modules/Sources/WordPressCore/Plugins/PluginDirectoryDataStore.swift
@@ -2,6 +2,8 @@ import Foundation
 import WordPressAPI
 import WordPressAPIInternal
 
+// MARK: - DataStore for full plugin deatils
+
 public typealias PluginDirectoryDataStoreQuery = InMemoryDataStore<PluginInformation>.Query
 public typealias InMemoryPluginDirectoryDataStore = InMemoryDataStore<PluginInformation>
 
@@ -20,5 +22,27 @@ extension PluginInformation: @retroactive Identifiable {
 extension InMemoryPluginDirectoryDataStore {
     func get(_ slug: PluginWpOrgDirectorySlug) async throws -> PluginInformation? {
         try await list(query: .slug(slug)).first
+    }
+}
+
+// MARK: - DataStore for plugin browser (featured, recommended plugins), which contains shortened `PluginInformation`
+
+public typealias CategorizedPluginInfomrationDataStoreQuery = InMemoryDataStore<CategorizedPluginInfomration>.Query
+public typealias CategorizedPluginInfomrationDataStore = InMemoryDataStore<CategorizedPluginInfomration>
+
+public struct CategorizedPluginInfomration: Identifiable, Sendable {
+    public var category: WordPressOrgApiPluginDirectoryCategory
+    public var plugins: [PluginInformation]
+
+    public var id: WordPressOrgApiPluginDirectoryCategory { category }
+}
+
+extension CategorizedPluginInfomrationDataStore.Query {
+    public static func category(_ category: WordPressOrgApiPluginDirectoryCategory) -> CategorizedPluginInfomrationDataStore.Query {
+        .init(sortBy: nil) { $0.category == category }
+    }
+
+    public static func category(_ categories: Set<WordPressOrgApiPluginDirectoryCategory>) -> CategorizedPluginInfomrationDataStore.Query {
+        .init(sortBy: nil) { categories.contains($0.category) }
     }
 }
