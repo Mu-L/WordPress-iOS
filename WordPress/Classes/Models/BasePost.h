5966b4aecab3ae11258f6c3c225780dd89d0eb32
Gio Lodi <gio@mokacoding.com>
2025-03-13 09:56:52 +1100

    Move `PostContentProvider.h` to WordPressData

--- WordPress/Classes/Models/BasePost.h
+++ WordPress/Classes/Models/BasePost.h
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 #import "Blog.h"
-#import "PostContentProvider.h"
+@import WordPressData;
 
 NS_ASSUME_NONNULL_BEGIN
 
--- WordPress/Classes/Models/Comment/Comment+CoreDataClass.swift
+++ WordPress/Classes/Models/Comment/Comment+CoreDataClass.swift
@@ -1,5 +1,6 @@
 import Foundation
 import CoreData
+import WordPressData
 
 @objc(Comment)
 public class Comment: NSManagedObject {
