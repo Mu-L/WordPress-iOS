0a0026699bfbfbef7d52d35845c03cf31290a60d
kean <grebenyuk.alexander@gmail.com>
2025-03-12 09:07:10 -0400

    Extract BuildSettings to separate files

---
+++ Targets/Jetpack/Sources/BuildSettings+Jetpack.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+extension BuildSettings {
+    static func make() -> BuildSettings {
+        let configuration = XcodeBuildConfiguration.current
+        return BuildSettings(
+            itunesAppID: "1565481562",
+            pushNotificationAppID: configuration.pushNotificationAppID
+        )
+    }
+}
+
+private enum XcodeBuildConfiguration {
+    case debug
+    case `internal`
+    case alpha
+    case release
+
+#if DEBUG
+    static let current: XcodeBuildConfiguration = .debug
+#elseif INTERNAL_BUILD
+    static let current: XcodeBuildConfiguration = .internal
+#elseif ALPHA_BUILD
+    static let current: XcodeBuildConfiguration = .current
+#else
+    static let current: XcodeBuildConfiguration = .release
+#endif
+
+    var pushNotificationAppID: String {
+        switch self {
+        case .debug: "com.jetpack.appstore.dev"
+        case .internal: "com.jetpack.internal"
+        case .alpha: "com.jetpack.alpha"
+        case .release: "com.jetpack.appstore"
+        }
+    }
+}
--- Targets/WordPress/Sources/AppDelegate+WordPress.swift
+++ Targets/WordPress/Sources/AppDelegate+WordPress.swift
@@ -2,36 +2,6 @@ import UIKit
 
 class AppDelegate: WordPressAppDelegate {
     override func makeBuildSettings() -> BuildSettings {
-        let configuration = XcodeBuildConfiguration.current
-        return BuildSettings(
-            itunesAppID: "335703880",
-            pushNotificationAppID: configuration.pushNotificationAppID
-        )
-    }
-}
-
-private enum XcodeBuildConfiguration {
-    case debug
-    case `internal`
-    case alpha
-    case release
-
-#if DEBUG
-    static let current: XcodeBuildConfiguration = .debug
-#elseif INTERNAL_BUILD
-    static let current: XcodeBuildConfiguration = .internal
-#elseif ALPHA_BUILD
-    static let current: XcodeBuildConfiguration = .current
-#else
-    static let current: XcodeBuildConfiguration = .release
-#endif
-
-    var pushNotificationAppID: String {
-        switch self {
-        case .debug: "org.wordpress.appstore.dev"
-        case .internal: "org.wordpress.internal"
-        case .alpha: "org.wordpress.alpha"
-        case .release: "org.wordpress.appstore"
-        }
+        BuildSettings.make()
     }
 }
