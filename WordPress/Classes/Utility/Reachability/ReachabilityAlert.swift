d17448e949fd58ba4991db940b699da4fe0313ce
Gio Lodi <gio@mokacoding.com>
2025-03-20 21:00:33 +1100

    Port `ReachabilityAlert` to Swift

---
+++ WordPress/Classes/Utility/Reachability/ReachabilityAlert.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+private var currentReachabilityAlert: _ReachabilityAlert?
+
+class _ReachabilityAlert: NSObject {
+
+    let retryBlock: (() -> Void)?
+
+    init(retryBlock: (() -> Void)?) {
+        self.retryBlock = retryBlock
+    }
+
+    func show() {
+        guard currentReachabilityAlert == nil else { return }
+
+        let title = NSLocalizedString("reachability-utils.alert.title", value: "No Connection", comment: "")
+        let message = "" // TODO:
+        let cancelActionTitle = NSLocalizedString("reachability-utils.alert.cancel", value: "OK", comment: "")
+        let retryActionTitle = NSLocalizedString("reachability-utils.alert.retry", value: "Retry?", comment: "")
+
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+
+        alertController.addCancelActionWithTitle(cancelActionTitle) { _ in
+            currentReachabilityAlert = nil
+        }
+
+        if let retryBlock {
+            alertController.addDefaultActionWithTitle(retryActionTitle) { _ in
+                currentReachabilityAlert = nil
+                retryBlock()
+            }
+        }
+
+        // Note: This viewController might not be visible anymore
+        alertController.presentFromRootViewController()
+
+        currentReachabilityAlert = self;
+    }
+}
