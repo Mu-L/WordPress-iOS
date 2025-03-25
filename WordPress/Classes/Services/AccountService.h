3707db56a79ae6ea60d4dc217d13c0fe225e47b8
kean <grebenyuk.alexander@gmail.com>
2025-03-25 15:26:48 -0400

    Add missing CoreDataService imports

--- WordPress/Classes/Services/AccountService.h
+++ WordPress/Classes/Services/AccountService.h
@@ -1,5 +1,5 @@
 #import <Foundation/Foundation.h>
-
+#import "CoreDataService.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
--- WordPress/Classes/Services/BlogService.h
+++ WordPress/Classes/Services/BlogService.h
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 #import "Blog.h"
-
+#import "CoreDataService.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
