ceb9381e3913e2f57d71307a9f70d3d06673fc18
kean <grebenyuk.alexander@gmail.com>
2025-03-25 14:07:21 -0400

    Fix which Keystone.h is used

--- WordPress/Keystone/Keystone.h
+++ WordPress/Keystone/Keystone.h
@@ -17,3 +17,85 @@ FOUNDATION_EXPORT const unsigned char KeystoneVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <Keystone/PublicHeader.h>
 
 
+#import "ActivityLogViewController.h"
+#import "AccountService.h"
+
+#import "Blog.h"
+#import "BlogService.h"
+#import "BlogSyncFacade.h"
+#import "BlogDetailsViewController.h"
+
+#import "CommentService.h"
+#import "CommentsViewController+Network.h"
+#import "Constants.h"
+
+#import "LocalCoreDataService.h"
+
+#import "Media.h"
+#import "MediaService.h"
+#import "MenuItem.h"
+#import "MenuItemsViewController.h"
+#import "MenusService.h"
+#import "MenusViewController.h"
+#import "Media+Extensions.h"
+
+#import "NSObject+Helpers.h"
+
+#import "PageSettingsViewController.h"
+#import "PostContentProvider.h"
+#import "PostCategory.h"
+#import "PostCategoryService.h"
+#import "PostContentProvider.h"
+#import "PostHelper.h"
+#import "PostMetaButton.h"
+#import "PostService.h"
+#import "PostServiceOptions.h"
+#import "PostSettingsViewController.h"
+#import "PostSettingsViewController_Internal.h"
+#import "PostTag.h"
+#import "PostTagService.h"
+
+#import "ReachabilityUtils.h"
+#import "ReaderGapMarker.h"
+#import "ReaderPost.h"
+#import "ReaderPostService.h"
+#import "ReaderSiteService.h"
+#import "ReaderSiteService_Internal.h"
+#import "ReaderTopicService.h"
+
+#import "SettingsSelectionViewController.h"
+#import "SettingsMultiTextViewController.h"
+#import "SettingTableViewCell.h"
+#import "SettingsTextViewController.h"
+#import "SharingViewController.h"
+#import "SiteSettingsViewController.h"
+#import "SourcePostAttribution.h"
+#import "StatsViewController.h"
+#import "SuggestionsTableView.h"
+#import "SuggestionsTableViewCell.h"
+#import "SVProgressHUD+Dismiss.h"
+
+#import "Theme.h"
+#import "ThemeService.h"
+
+#import "UIAlertControllerProxy.h"
+#import "UIApplication+Helpers.h"
+#import "UIView+Subviews.h"
+
+#import "WPAccount.h"
+#import "WPActivityDefaults.h"
+#import "WPAnalyticsTrackerWPCom.h"
+#import "WPAppAnalytics.h"
+#import "WPAnalyticsTrackerAutomatticTracks.h"
+#import "WPAuthTokenIssueSolver.h"
+#import "WPUploadStatusButton.h"
+#import "WPError.h"
+#import "WPStyleGuide+Pages.h"
+#import "WPStyleGuide+WebView.h"
+#import "WPTableViewHandler.h"
+#import "WPWebViewController.h"
+#import "WPTabBarController.h"
+#import "WPLogger.h"
+#import "WPException.h"
+
+FOUNDATION_EXTERN void SetCocoaLumberjackObjCLogLevel(NSUInteger ddLogLevelRawValue);
