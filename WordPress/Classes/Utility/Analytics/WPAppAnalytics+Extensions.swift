a2302988df5684dce5188aa7808480c6204d9698
kean <grebenyuk.alexander@gmail.com>
2025-03-26 10:25:03 -0400

    Optimize blog-related tracking to avoid constant blog lookup by ID

--- WordPress/Classes/Utility/Analytics/WPAppAnalytics+Extensions.swift
+++ WordPress/Classes/Utility/Analytics/WPAppAnalytics+Extensions.swift
@@ -3,7 +3,13 @@ import WordPressShared
 
 extension WPAppAnalytics {
     static func track(_ stat: WPAnalyticsStat, properties: [String: Any]? = nil, blog: Blog) {
-        track(stat, properties: properties, blogID: blog.dotComID)
+        var properties = properties ?? [:]
+        if let blogID = blog.dotComID {
+            properties[WPAppAnalyticsKeyBlogID] = blogID
+        }
+        properties[WPAppAnalyticsKeySiteType] = siteType(for: blog)
+
+        WPAppAnalytics.track(stat, withProperties: properties)
     }
 
     static func track(_ stat: WPAnalyticsStat, properties: [String: Any]? = nil, blogID: NSNumber?) {
@@ -17,24 +23,22 @@ extension WPAppAnalytics {
     }
 
     private static func _track(_ stat: WPAnalyticsStat, properties: [String: Any]? = nil, blogID: NSNumber?) {
+        wpAssert(Thread.isMainThread)
+
         var properties = properties ?? [:]
         if let blogID {
             properties[WPAppAnalyticsKeyBlogID] = blogID
-            properties[WPAppAnalyticsKeySiteType] = siteType(forBlogID: blogID)
+
+            let context = ContextManager.shared.mainContext
+            if let blog = Blog.lookup(withID: blogID, in: context) {
+                properties[WPAppAnalyticsKeySiteType] = siteType(for: blog)
+            }
         }
-        if properties.isEmpty {
-            WPAppAnalytics.track(stat)
-        } else {
-            WPAppAnalytics.track(stat, withProperties: properties)
-        }
+
+        WPAppAnalytics.track(stat, withProperties: properties)
     }
 
-    private static func siteType(forBlogID blogID: NSNumber) -> String {
-        wpAssert(Thread.isMainThread)
-        let context = ContextManager.shared.mainContext
-        if let blog = Blog.lookup(withID: blogID, in: context), blog.isWPForTeams() {
-            return WPAppAnalyticsValueSiteTypeP2
-        }
-        return WPAppAnalyticsValueSiteTypeBlog
+    private static func siteType(for blog: Blog) -> String {
+        blog.isWPForTeams() ? WPAppAnalyticsValueSiteTypeP2 : WPAppAnalyticsValueSiteTypeBlog
     }
 }
