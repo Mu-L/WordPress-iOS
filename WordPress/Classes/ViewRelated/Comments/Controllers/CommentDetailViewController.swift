ad270ce4cb3bfb82b50c1de8e4f923dc2b9c3fcb
Gio Lodi <gio@mokacoding.com>
2025-04-03 20:19:16 +1100

    Disambiguate `Notification` usages — `/ Notification,$/`

--- WordPress/Classes/ViewRelated/Comments/Controllers/CommentDetailViewController.swift
+++ WordPress/Classes/ViewRelated/Comments/Controllers/CommentDetailViewController.swift
@@ -218,7 +218,7 @@ class CommentDetailViewController: UIViewController, NoResultsViewHost {
     }
 
     init(comment: Comment,
-         notification: Notification,
+         notification: WordPressData.Notification,
          notificationDelegate: CommentDetailsNotificationDelegate?,
          managedObjectContext: NSManagedObjectContext = ContextManager.shared.mainContext) {
         self.comment = comment
