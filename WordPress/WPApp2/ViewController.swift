c50566a22a7a0a21166bdb656b79f7fe5dcd61eb
kean <grebenyuk.alexander@gmail.com>
2025-04-04 09:44:56 -0400

    Add two experimental targes

---
+++ WordPress/WPApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WPApp2
+//
+//  Created by Alex on 4/4/25.
+//  Copyright © 2025 WordPress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
