ac93eab5d64ad2b7f747a5b657a8965d9239aa95
Tony Li <tony.li@automattic.com>
2025-11-14 13:33:34 +1300

    Remove `dotComWebLoginEnabled` (#24977)
    * Remove `dotComWebLoginEnabled`
    
    * Remove WP.com login UI tests
    
    * Update self-hosted site login tests
    
    * Remove SupportScreenTests
    
    After moving to web login, the tests are not longer applicable.

--- WordPress/UITests/Tests/LoginTests.swift
+++ WordPress/UITests/Tests/LoginTests.swift
@@ -1,45 +1,12 @@
 import UITestsFoundation
 import XCTest
 
+@MainActor
 class LoginTests: XCTestCase {
-
-    @MainActor
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        setUpTestSuite()
-    }
-
-    // Unified email login/out
-    func testWPcomLoginLogout() throws {
-        try PrologueScreen()
-            .selectContinue()
-            .proceedWith(email: WPUITestCredentials.testWPcomUserEmail)
-            .proceedWithValidPassword()
-        try makeMainNavigationComponent()
-            .goToMeScreen()
-            .logoutToPrologue()
-            .assertScreenIsLoaded()
-    }
-
-    /**
-     This test opens safari to trigger the mocked magic link redirect
-     */
-    func testEmailMagicLinkLogin() throws {
-        try WelcomeScreen()
-            .selectLogin()
-            .selectEmailLogin()
-            .proceedWith(email: WPUITestCredentials.testWPcomUserEmail)
-            .proceedWithLink()
-            .openMagicLoginLink()
-
-        try makeMainNavigationComponent()
-            .goToMeScreen()
-            .logout()
-            .assertScreenIsLoaded()
-    }
 
     // Unified self hosted login/out
     func testSelfHostedLoginLogout() throws {
+        setUpTestSuite()
         try PrologueScreen()
             .selectSiteAddress()
             .proceedWith(siteAddress: WPUITestCredentials.selfHostedSiteAddress)
@@ -59,24 +26,13 @@ class LoginTests: XCTestCase {
             .assertScreenIsLoaded()
     }
 
-    // Unified WordPress.com email login failure due to incorrect password
-    func testWPcomInvalidPassword() throws {
-        try PrologueScreen()
-            .selectContinue()
-            .proceedWith(email: WPUITestCredentials.testWPcomUserEmail)
-            .proceedWithInvalidPassword()
-            .verifyLoginError()
-    }
-
     // Self-Hosted after WordPress.com login.
     // Login to a WordPress.com account, open site switcher, then add a self-hosted site.
     func testAddSelfHostedSiteAfterWPcomLogin() throws {
-        try PrologueScreen()
-            .selectContinue()
-            .proceedWith(email: WPUITestCredentials.testWPcomUserEmail)
-            .proceedWithValidPassword()
+        setUpTestSuite(selectWPComSite: WPUITestCredentials.testWPcomFreeSite)
 
-            // From here, bring up the sites list and choose to add a new self-hosted site.
+        // From here, bring up the sites list and choose to add a new self-hosted site.
+        try MySiteScreen()
             .showSiteSwitcher()
             .addSelfHostedSite()
 
