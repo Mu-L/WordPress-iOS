6abfdc7f7d4c596c81da012efc40f0b56fdb4957
Tony Li <tony.li@automattic.com>
2026-01-14 10:56:04 +1300

    Redirect to WP.com login during self-hosted sites login (#25118)
    * Redirect to WP.com login during self-hosted sites login
    
    * Use WordPressKit to check if a site is hosted on WP.com
    
    * Remove an unused code
    
    * Show a notice before redirecting to WP.com login

--- WordPress/Classes/Login/LoginWithUrlView.swift
+++ WordPress/Classes/Login/LoginWithUrlView.swift
@@ -10,6 +10,7 @@ struct LoginWithUrlView: View {
 
     weak var presenter: UIViewController?
     let loginCompleted: (TaggedManagedObjectID<Blog>) -> Void
+    let presentDotComLogin: () -> Void
 
     @State fileprivate var errorMessage: String?
     @State private var urlField: String = ""
@@ -30,7 +31,7 @@ struct LoginWithUrlView: View {
                 .padding(24)
                 .frame(maxWidth: .infinity, alignment: .center)
 
-            Text(Self.enterSiteAddress)
+            Text(Strings.enterSiteAddress)
 
             siteAdddressTextField()
 
@@ -50,7 +51,7 @@ struct LoginWithUrlView: View {
             .disabled(isContinueButtonDisabled)
         }
         .padding()
-        .navigationTitle(Self.title)
+        .navigationTitle(Strings.title)
         .navigationBarTitleDisplayMode(.inline)
     }
 
@@ -89,7 +90,16 @@ struct LoginWithUrlView: View {
                 dismiss()
                 self.loginCompleted(blog)
             } catch {
-                errorMessage = error.localizedDescription
+                if await shouldRedirectToDotComLogin(error: error) {
+                    // We need to chain the dismissing and presenting,
+                    // which is not supported by SwiftUI's `dismiss` variable.
+                    presenter.dismiss(animated: true) {
+                        Notice(title: Strings.wpcomSiteRedirect).post()
+                        presentDotComLogin()
+                    }
+                } else {
+                    errorMessage = error.localizedDescription
+                }
             }
 
             isLoading = false
@@ -99,15 +109,68 @@ struct LoginWithUrlView: View {
             await login()
         }
     }
+
+    // If the error is "API root (wp-json) not found", it's possible that the user typed
+    // a WP.com simple site address. We should redirect to WP.com login if that's
+    // the case.
+    private func shouldRedirectToDotComLogin(
+        error: SelfHostedSiteAuthenticator.SignInError
+    ) async -> Bool {
+        guard case let .authentication(error) = error,
+              let error = error as? AutoDiscoveryAttemptFailure,
+              error.shouldAttemptDotComLogin else { return false}
+
+        let api = WordPressComRestApi.anonymousApi(userAgent: WPUserAgent.defaultUserAgent())
+        let remote = BlogServiceRemoteREST(wordPressComRestApi: api, siteID: 0)
+        let url = WordPressAuthenticator.baseSiteURL(string: urlField)
+        let response: [AnyHashable: Any]? = await withCheckedContinuation { continuation in
+            remote.fetchUnauthenticatedSiteInfo(forAddress: url) {
+                continuation.resume(returning: $0)
+            } failure: { _ in
+                continuation.resume(returning: nil)
+            }
+        }
+        return (response?["isWordPressDotCom"] as? Bool) == true
+    }
 }
 
-private extension LoginWithUrlView {
-    static var title: String { NSLocalizedString("addSite.selfHosted.title", value: "Add Self-Hosted Site", comment: "Title of the page to add a self-hosted site") }
-    static var enterSiteAddress: String { NSLocalizedString("addSite.selfHosted.enterSiteAddress", value: "Enter the address of the WordPress site you'd like to connect.", comment: "A message to inform users to type the site address in the text field.") }
+private enum Strings {
+    static let title = NSLocalizedString(
+        "addSite.selfHosted.title",
+        value: "Add Self-Hosted Site",
+        comment: "Title of the page to add a self-hosted site"
+    )
+
+    static let enterSiteAddress = NSLocalizedString(
+        "addSite.selfHosted.enterSiteAddress",
+        value: "Enter the address of the WordPress site you'd like to connect.",
+        comment: "A message to inform users to type the site address in the text field."
+    )
+
+    static let wpcomSiteRedirect = NSLocalizedString(
+        "addSite.selfHosted.wpcomSiteRedirect",
+        value: "This site is hosted on WordPress.com. Please log in with your WordPress.com account.",
+        comment: "Notice message shown when a user tries to add a WordPress.com site as self-hosted"
+    )
+}
+
+private extension AutoDiscoveryAttemptFailure {
+    var shouldAttemptDotComLogin: Bool {
+        switch self {
+        case .ParseSiteUrl:
+            false
+        case .FindApiRoot, .FetchAndParseApiRoot:
+            true
+        }
+    }
 }
 
 // MARK: - SwiftUI Preview
 
 #Preview {
-    LoginWithUrlView(presenter: nil) { _ in }
+    LoginWithUrlView(
+        presenter: nil,
+        loginCompleted: { _ in },
+        presentDotComLogin: { }
+    )
 }
